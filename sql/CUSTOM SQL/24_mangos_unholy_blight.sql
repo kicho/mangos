8f697b0ba28aa369fcd6b4e8ed9aa089a04f3234
kicho <deathlord22@gmail.com>
2010-07-18 18:23:21 +0200

    Corrected SQL files.

--- sql/CUSTOM SQL/24_mangos_unholy_blight.sql
+++ sql/CUSTOM SQL/24_mangos_unholy_blight.sql
@@ -1,7 +1,6 @@
 ## Unholy Blight
 DELETE FROM spell_proc_event WHERE entry = 49194;
-INSERT INTO spell_proc_event VALUES
-(49194, 0x00, 15, 0x00002000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0.000000, 0.000000, 0);
+INSERT INTO `spell_proc_event` VALUES ('49194', '0', '15', '8192', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0');
 
 DELETE FROM spell_bonus_data WHERE entry = 50536;
 INSERT INTO spell_bonus_data VALUES
