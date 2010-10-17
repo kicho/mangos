a622f0ccc9baa36a7e9ce452cb6472a406ee75d6
kicho <deathlord22@gmail.com>
2010-10-18 00:29:34 +0200

    SQL typos

--- sql/CUSTOM SQL/1_mangos_announce_system.sql
+++ sql/CUSTOM SQL/1_mangos_announce_system.sql
@@ -9,4 +9,4 @@ INSERT INTO `mangos_string` VALUES
 (67,'|c1f40af20 <Admin>|cffff0000[%s]|c1f40af20 announce:|cffffff00 %s|r',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL), 
 (68,'|c1f40af20 <Dev>|cffff0000[%s]|c1f40af20 announce:|cffffff00 %s|r',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL), 
 (69,'|cffffcc00 <Owner>|cffff0000[%s]|c1f40af20 announce:|cffffff00 %s|r',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL), 
-(70,'|cffffff00 <SERVER> announcement:|cffffff00 %s|r',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+(70,'|cffffff00 <SERVER> announcement:|cffffff00 %s|r',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
